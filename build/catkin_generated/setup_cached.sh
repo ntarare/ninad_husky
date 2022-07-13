6569db66b59d6ad48ecd1b4ae8bec3bdeadaae5e
ntarare <ntarare@g.clemson.edu>
2022-07-13 09:40:25 -0400

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ninad/git_ws/ninad_husky/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/ninad/git_ws/ninad_husky/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ninad/git_ws/ninad_husky/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/ninad/git_ws/ninad_husky/src:$ROS_PACKAGE_PATH"
